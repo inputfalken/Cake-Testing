c6856cea432ed05b452309b4af3d68d090c1c811
Robert Andersson <inputfalken@gmail.com>
2017-10-29 01:51:09 +0200

    Add test project

---
+++ Tests/CoffeeTests.cs
@@ -0,0 +1,24 @@
+﻿using Store;
+using Xunit;
+
+namespace Tests {
+    public class CoffeeTests {
+        [Fact]
+        public void Large_Coffee_Is_Expected_Volume() {
+            var coffee = new Coffee(20, Size.Large, "Latte", "Arabica");
+            Assert.Equal(370d, coffee.Centiliters);
+        }
+
+        [Fact]
+        public void Medium_Coffee_Is_Expected_Volume() {
+            var coffee = new Coffee(20, Size.Medium, "Latte", "Arabica");
+            Assert.Equal(370d, coffee.Centiliters);
+        }
+
+        [Fact]
+        public void Small_Coffee_Is_Expected_Volume() {
+            var coffee = new Coffee(20, Size.Small, "Latte", "Arabica");
+            Assert.Equal(370d, coffee.Centiliters);
+        }
+    }
+}
