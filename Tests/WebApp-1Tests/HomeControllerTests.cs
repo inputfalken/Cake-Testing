79949b08983ab7bef15b97e2d0cd51e364bc9c1b
Robert Andersson <inputfalken@gmail.com>
2017-10-29 14:59:38 +0100

    Add tests for webapp1

---
+++ Tests/WebApp-1Tests/HomeControllerTests.cs
@@ -0,0 +1,14 @@
+﻿using Cake.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Xunit;
+
+namespace WebApp_1Tests {
+    public class HomeControllerTests {
+        [Fact]
+        public void Test1() {
+            HomeController sut = new HomeController();
+            IActionResult result = sut.Index();
+            Assert.IsType<ViewResult>(result);
+        }
+    }
+}
